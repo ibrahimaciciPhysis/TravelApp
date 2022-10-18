ac3ad121fad5043a09b17c022517f0c220a2645e
MibrahimAcici <ibrahimacici@gmail.com>
2022-10-18 14:48:50 +0300

    Travel_App

---
+++ app/src/main/java/com/ibrahim/travelapp/presentation/search/SearchAdapter.kt
@@ -0,0 +1,49 @@
+package com.ibrahim.travelapp.presentation.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ibrahim.travelapp.databinding.SearchAttractionsItemBinding
+import com.ibrahim.travelapp.databinding.SearchItemBinding
+import com.ibrahim.travelapp.domain.model.alltravel.AllTravelItem
+
+class SearchAdapter() : RecyclerView.Adapter<SearchAdapter.SearchVH>() {
+
+    private var allTravelItemList = mutableListOf<AllTravelItem?>()
+
+    fun setList(newList: List<AllTravelItem?>?) {
+        this.allTravelItemList.clear()
+        this.allTravelItemList.addAll(newList?: emptyList())
+        notifyDataSetChanged()
+    }
+
+    inner class SearchVH(val binding: SearchItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchVH {
+        return SearchVH(SearchItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: SearchVH, position: Int) {
+        val travel=allTravelItemList[position]
+
+        Glide.with(holder.binding.searchItemImageview)
+            .load(travel?.images?.get(2)?.url.toString())
+            .into(holder.binding.searchItemImageview)
+
+        holder.binding.searchItemTextviewCity.text = travel?.city.toString()
+        holder.binding.searchItemTextviewCountry.text = travel?.country.toString()
+
+        holder.binding.root.setOnClickListener {
+            val imageUrl = travel?.images?.get(2)?.url.toString()
+            val action= SearchFragmentDirections.actionSearchFragmentToDetailFragment(travel,imageUrl)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+    override fun getItemCount(): Int = allTravelItemList.size
+
+}
--- app/src/main/java/com/ibrahim/travelapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/ibrahim/travelapp/presentation/search/SearchFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ibrahim.travelapp.databinding.FragmentSearchBinding
+import com.ibrahim.travelapp.domain.model.alltravel.AllTravel
 import com.ibrahim.travelapp.domain.model.alltravel.AllTravelItem
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,9 +21,11 @@ class SearchFragment :Fragment() {
 
     lateinit var searchDestinationAdapter: SearchDestinationAdapter
     lateinit var searchAttractionAdapter: SearchAttractionAdapter
+    lateinit var searchAdapter: SearchAdapter
     private val searchViewModel: SearchViewModel by viewModels()
     private var topDestinationList: List<AllTravelItem> = arrayListOf()
     private var nearbyList: List<AllTravelItem> = arrayListOf()
+    private var travelList: List<AllTravelItem> = arrayListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         fragmentSearchBinding = FragmentSearchBinding.inflate(inflater, container, false)
@@ -40,33 +44,63 @@ class SearchFragment :Fragment() {
     }
 
     private fun initClickListeners() {
+        fragmentSearchBinding.searchEdittextWhere.doAfterTextChanged { it->
+            it?.let { search->
+                fragmentSearchBinding.apply {
+                if (search.isNotEmpty()){
+                    searchRecyclerviewSearch.visibility = View.VISIBLE
+                    searchTextviewTop.visibility = View.INVISIBLE
+                    searchRecyclerviewDestinations.visibility = View.INVISIBLE
+                    searchTextviewNearby.visibility = View.INVISIBLE
+                    searchRecyclerviewAttractions.visibility = View.INVISIBLE
+                    searchAdapter.setList(travelList.filter { it.title?.lowercase()?.startsWith(search.toString().lowercase()) == true })
+                }else{
+                    searchRecyclerviewSearch.visibility = View.INVISIBLE
+                    searchTextviewTop.visibility = View.VISIBLE
+                    searchRecyclerviewDestinations.visibility = View.VISIBLE
+                    searchTextviewNearby.visibility = View.VISIBLE
+                    searchRecyclerviewAttractions.visibility = View.VISIBLE
+                }
+                }
+            }
+        }
 
     }
 
 
     private fun observeTravelList() {
-        searchViewModel.getAllTravel().observe(viewLifecycleOwner) { travelList ->
-            topDestinationList= travelList.filter { it.category == "topdestination" }
-            nearbyList= travelList.filter { it.category == "nearby" }
+        searchViewModel.getAllTravel().observe(viewLifecycleOwner) { allTravelList ->
+            topDestinationList= allTravelList.filter { it.category == "topdestination" }
+            nearbyList = allTravelList.filter { it.category == "nearby" }
+            travelList = allTravelList
             searchDestinationAdapter.setList(topDestinationList)
             searchAttractionAdapter.setList(nearbyList)
+            //searchAdapter.setList(travelList)
+            fragmentSearchBinding.searchTextviewTop.isVisible = true
+            fragmentSearchBinding.searchTextviewNearby.isVisible = true
+            fragmentSearchBinding.searchRecyclerviewSearch.isVisible = false
             fragmentSearchBinding.searchRecyclerviewDestinations.isVisible = true
             fragmentSearchBinding.searchRecyclerviewAttractions.isVisible = true
             fragmentSearchBinding.searchProgressDestinations.isVisible = false
             fragmentSearchBinding.searchProgressAttractions.isVisible = false
+
         }
     }
 
     private fun initAdapter() {
         searchDestinationAdapter = SearchDestinationAdapter()
         searchAttractionAdapter= SearchAttractionAdapter()
+        searchAdapter= SearchAdapter()
         fragmentSearchBinding.searchRecyclerviewDestinations.adapter = searchDestinationAdapter
         fragmentSearchBinding.searchRecyclerviewAttractions.adapter = searchAttractionAdapter
+        fragmentSearchBinding.searchRecyclerviewSearch.adapter = searchAdapter
         //Adapterin Ekrandaki Görünümü
         val layoutManagerDestination = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
         val layoutManagerAttraction = LinearLayoutManager(context)
+        val layoutManager = LinearLayoutManager(context)
         fragmentSearchBinding.searchRecyclerviewDestinations.layoutManager = layoutManagerDestination
         fragmentSearchBinding.searchRecyclerviewAttractions.layoutManager = layoutManagerAttraction
+        fragmentSearchBinding.searchRecyclerviewSearch.layoutManager = layoutManager
     }
 
     private fun fetchTravel() {
